9e4d8cd92fb0f7d81593471c31545da59326febb
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2025-09-03 01:41:32 -0300

    Se agrego clase Articulo y clase Imagen

---
+++ Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTN_FRGP_ActividadII
+{
+    internal class Articulo
+    {
+       
+        
+        public int CodArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public List<Imagen> Imagenes { get; set } = new List<Imagen>();
+
+        
+    }
+}
