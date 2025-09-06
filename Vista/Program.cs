15dd85d76e27f7fedbd22d3310a96dc760a3e52f
SebastianDte <sebateunmate@outlook.com>
2025-09-06 00:20:01 -0300

    Lógica para filtro rapido,avanzado, nuevas validaciones agregadas

--- Vista/Program.cs
+++ Vista/Program.cs
@@ -16,7 +16,7 @@ namespace Vista
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMarcas());
+            Application.Run(new Form1());
         }
     }
 }
