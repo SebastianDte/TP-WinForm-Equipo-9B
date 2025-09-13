f96eaf63e2394ce5890249705a154288ac5ae86d
SebastianDte <sebateunmate@outlook.com>
2025-09-13 00:22:44 -0300

    Formulario cateogira agregado

--- Vista/frmCategorias.cs
+++ Vista/frmCategorias.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,29 @@ using System.Windows.Forms;
 
 namespace Vista
 {
-    public partial class frmCategorias : Form
+    public partial class frmCategorias : MaterialForm
     {
         public frmCategorias()
         {
             InitializeComponent();
+            ConfigurarMaterialSkin();
+
+        }
+
+
+
+        private void ConfigurarMaterialSkin()
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.BlueGrey800,
+                Primary.BlueGrey900,
+                Primary.BlueGrey500,
+                Accent.LightBlue700,
+                TextShade.WHITE
+            );
         }
 
 
